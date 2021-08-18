155dd76e4c25469c11f25c7f4b1233a344a153e0
CaneteDave <drcanete@student.hau.edu.ph>
2021-08-18 15:39:14 +0800

    Initial commit

---
+++ src/Test1/Test2.java
@@ -0,0 +1,13 @@
+package Test1;
+
+public class Test2 {
+    public String getTest_123() {
+        return Test_123;
+    }
+
+    public void setTest_123(String test_123) {
+        Test_123 = test_123;
+    }
+
+    public String Test_123;
+}
